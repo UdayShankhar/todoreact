c77f0dafdf4d5a617fa23884c8ced74f75ac22b1
Uday <shankhar87@gmail.com>
2022-02-20 21:11:16 +0530

    message

---
+++ src/Filter.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Filter({ onFilterChange }) {
+    const filters = ["all", "active", "completed"];
+    const clickHandler = (currentValue) => {
+        onFilterChange(currentValue);
+    }
+    return (
+        <div className="filters">
+        <ul className="nav nav-pills todo-nav">
+            {filters.map(item => {
+                return (<li
+                    key={item}
+                    role="presentation"
+                    className="nav-item all-task active"
+                    onClick={() => clickHandler(item)}
+                >
+                    <a className="nav-link" style={{ cursor: 'pointer', textTransform: 'capitalize' }}>
+                        {item}
+                    </a>
+                </li>)
+            })}
+        </ul>
+        </div >
+        )
+}
